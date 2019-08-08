c1a1eea5f357511b7aba3cfb96402cdcd6d5f34a
JiemingLi <1207734468@qq.com>
2019-08-08 15:07:26 +0800

    Spring-Cloud-Ad

---
+++ jieming-ad-service/ad-search/src/main/java/com/jieming/ad/mysql/dto/MySqlRowData.java
@@ -0,0 +1,20 @@
+package com.jieming.ad.mysql.dto;
+
+import com.jieming.ad.mysql.constant.OpType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MySqlRowData {
+    private String tableName;
+    private String level;
+    private OpType opType;
+    //字段索引到字段具体的名字的映射
+    private List<Map<String,String>> fieldValueMap = new ArrayList<>();
+}
